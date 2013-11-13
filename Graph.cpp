428af842117a2e9701c6ed1c1a1f9fd758013c4e
kojsmn <kojsmn@miamioh.edu>
2013-11-13 08:39:07 -0500

    add

--- Graph.cpp
+++ Graph.cpp
@@ -8,11 +8,13 @@
 Graph::Graph(unsigned int numNodes){
   //TODO
 	int* adjList = new int[numNodes];
-	
+	int* edgeList = new int[1];	
+	Edge();
 }
 
 int Graph::getCost(int node1, int node2){
-  //TODO	
+  //TODO 	
+
   return -1;
 }
 
@@ -20,6 +22,15 @@ int Graph::getCost(int node1, int node2){
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
   //TODO
+	if (cost < 0)
+		throw std::string("cost < 0");
+	Edge(cost,node2);
+	Edge(cost, node1);
+
+	//if (cost < 0)
+	//for (int i = 0; i < numVertices; i++){
+		//if (adj[i].
+	//}
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
