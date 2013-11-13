d50f36d9b910d0d4303cde9f640e31c21c738231
continnd <continnd@EAS-BEN002-B2.it.muohio.edu>
2013-11-13 09:52:06 -0500

    Implemented everything. Need to fix getCost

--- Graph.cpp
+++ Graph.cpp
@@ -6,22 +6,55 @@
 #include "Graph.h"
 
 Graph::Graph(unsigned int numNodes){
-  //TODO
+  adjList.resize(numNodes);
 }
 
 int Graph::getCost(int node1, int node2){
-  //TODO
-  return -1;
+	for(int i = 0; i< adjList[node1].edgeList.size(); i++){
+		if(adjList[node1].edgeList[i].dest==node2)
+			return adjList[node1].edgeList[i].cost;
+	}
+	return -1;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+  if(cost<0)
+	  throw (std::string) "cost must be >= 0";
+  Edge* toAdd = new Edge(cost,node2);
+  adjList[node1].edgeList.push_back(*toAdd);
+  toAdd = new Edge(cost,node2);
+  adjList[node2].edgeList.push_back(*toAdd);
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+  int removeEdgeHere=-1;
+  int i = 0;
+  while(i<adjList[node1].edgeList.size()){
+	  if(adjList[node1].edgeList[i].dest=node2){
+		  removeEdgeHere = i;
+		  break;
+	  }
+	  i++;
+  }
+  if(removeEdgeHere!=-1){
+	adjList[node1].edgeList[removeEdgeHere]=Edge();
+  }
+
+  removeEdgeHere=-1;
+  i = 0;
+  while(i<adjList[node2].edgeList.size()){
+	  if(adjList[node2].edgeList[i].dest=node1){
+		  removeEdgeHere = i;
+		  break;
+	  }
+	  i++;
+  }
+
+  if(removeEdgeHere!=-1){
+	adjList[node2].edgeList[removeEdgeHere]=Edge();
+  }
 }
