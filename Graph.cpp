94d0445e890d40c35431e46f8d8077aaa101e9a4
db4soundman <db4soundman@gmail.com>
2013-11-14 18:46:12 -0500

    Removed unnecessary for loops after realizing a better way to add the data to the Graph.

--- Graph.cpp
+++ Graph.cpp
@@ -15,11 +15,9 @@ Graph::Graph(unsigned int numNodes){
 
 int Graph::getCost(int node1, int node2){
     Node* temp = &adjList[node1];
-    for (int i = 0; i<temp->edgeList.size(); i++) {
-        if (temp->edgeList[i].dest == node2) {
-            return temp->edgeList[i].cost;
+        if (temp->edgeList[node2].dest == node2) {
+            return temp->edgeList[node2].cost;
         }
-    }
 
   return -1;
 }
@@ -32,32 +30,18 @@ void Graph::addEdge(int node1, int node2, double cost){
     }
     
     else{
-        bool keepSearching = true;
-        bool alreadyExists = false;
         Node* temp = &adjList[node1];
-        for (int i = 0; i<temp->edgeList.size() && keepSearching; i++) {
-            if (temp->edgeList[i].dest == node2) {
-                temp->edgeList[i].cost = cost;
-                keepSearching = false;
-                alreadyExists = true;
+            if (temp->edgeList[node2].dest != -1) {
+                temp->edgeList[node2].cost = cost;
+                temp = &adjList[node2];
+                if(temp->edgeList[node1].dest != -1) {
+                    temp->edgeList[node1].cost = cost;
+                }
             }
-        }
-        keepSearching = true;
-        if (alreadyExists){
-        temp = &adjList[node2];
-        for (int i = 0; i<temp->edgeList.size() && keepSearching; i++) {
-            if (temp->edgeList[i].dest == node1) {
-                temp->edgeList[i].cost = cost;
-                keepSearching = false;
-                alreadyExists = true;
-            }
-        }
-      }
-        
         else{
-            temp->edgeList[temp->edgeList.size()-1] = Edge(cost, node2);
+            temp->edgeList[node2] = Edge(cost, node2);
             temp = &adjList[node2];
-            temp->edgeList[temp->edgeList.size()-1] = Edge(cost, node1);
+            temp->edgeList[node1] = Edge(cost, node1);
             
         }
     }
@@ -68,25 +52,15 @@ void Graph::addEdge(int node1, int node2, double cost){
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-    bool keepSearching = true;
-    bool alreadyExists = false;
     Node* temp = &adjList[node1];
-    for (int i = 0; i<temp->edgeList.size() && keepSearching; i++) {
-        if (temp->edgeList[i].dest == node2) {
-            temp->edgeList[i].cost = -1;
-            keepSearching = false;
-            alreadyExists = true;
-        }
-    }
-    keepSearching = true;
-    if (alreadyExists){
-        temp = &adjList[node2];
-        for (int i = 0; i<temp->edgeList.size() && keepSearching; i++) {
-            if (temp->edgeList[i].dest == node1) {
-                temp->edgeList[i].cost = -1;
-                keepSearching = false;
-                alreadyExists = true;
+    
+        if (temp->edgeList[node2].dest != -1) {
+            temp->edgeList[node2].cost = -1;
+            temp = &adjList[node2];
+            if (temp->edgeList[node1].dest != -1) {
+                temp->edgeList[node1].cost = -1;
             }
+
         }
+            
     }
-}
