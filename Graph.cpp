57df0d1b4468030a6349a51fde860bdf89925d8f
Sebastian Monnin <monninse@miamioh.edu>
2013-11-18 11:23:46 -0500

    remove done

--- Graph.cpp
+++ Graph.cpp
@@ -69,9 +69,24 @@ void Graph::addEdge(int node1, int node2, double cost){
 }
 }
 }
-
+// erase from http://www.cplusplus.com/reference/vector/vector/erase/
+// and from book
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+      int p=0;
+            while(p < adjList[node1].edgeList.size()){
+                if(adjList[node1].edgeList[p].dest==node2)
+            adjList[node1].edgeList.erase(adjList[node1].edgeList.begin()+p);
+                    
+            p++;
+        }
+    
+    int q=0;
+    while(q < adjList[node2].edgeList.size()){
+        if(adjList[node2].edgeList[q].dest==node1)
+            adjList[node2].edgeList.erase(adjList[node2].edgeList.begin()+q);
+            
+        q++;
+    }
 }
