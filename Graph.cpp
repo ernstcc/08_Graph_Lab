3bf2a4db0ffefd7ace6231d7dd9d72c2655b9171
YangLi1 <liy28@muohio.edu>
2013-12-05 18:58:53 -0500

    Implemented addEdge()

--- Graph.cpp
+++ Graph.cpp
@@ -23,7 +23,33 @@ int Graph::getCost(int node1, int node2){
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+  if(cost < 0){
+	throw (std::string)"Not a valid cost to update.";
+  }
+  else{
+	  // check node1's edges
+	  if(getCost(node1, node2) < 0){
+		adjList[node1].edgeList.push_back(*new Edge(cost, node2));
+	  }
+	  else{
+		  for(int i = 0; i < adjList[node1].edgeList.size(); i++){
+			if(adjList[node1].edgeList[i].dest == node2){
+				adjList[node1].edgeList[i].cost = cost;
+			}
+		  }
+	  }
+	  // check node2's edges
+	  if(getCost(node2, node1) < 0){
+		adjList[node2].edgeList.push_back(*new Edge(cost, node1));
+	  }
+	  else{
+		  for(int i = 0; i < adjList[node2].edgeList.size(); i++){
+			if(adjList[node2].edgeList[i].dest == node1){
+				adjList[node2].edgeList[i].cost = cost;
+			}
+		  }
+	  }
+  }
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
