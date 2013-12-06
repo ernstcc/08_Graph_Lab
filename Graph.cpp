86129d05a9b83f346835763b35d51805a2ba923d
YangLi1 <liy28@muohio.edu>
2013-12-05 19:26:32 -0500

    Implemented removeEdge() and revised addEdge()
    Haven't tested all three functions. Just pray they would work...

--- Graph.cpp
+++ Graph.cpp
@@ -28,7 +28,7 @@ void Graph::addEdge(int node1, int node2, double cost){
   }
   else{
 	  // check node1's edges
-	  if(getCost(node1, node2) < 0){
+	  if(getCost(node1, node2) == -1){
 		adjList[node1].edgeList.push_back(*new Edge(cost, node2));
 	  }
 	  else{
@@ -39,7 +39,7 @@ void Graph::addEdge(int node1, int node2, double cost){
 		  }
 	  }
 	  // check node2's edges
-	  if(getCost(node2, node1) < 0){
+	  if(getCost(node2, node1) == -1){
 		adjList[node2].edgeList.push_back(*new Edge(cost, node1));
 	  }
 	  else{
@@ -55,5 +55,24 @@ void Graph::addEdge(int node1, int node2, double cost){
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+  if(getCost(node1, node2) != -1){
+	int count = 0;
+	while(count < adjList[node1].edgeList.size()){
+		if(adjList[node1].edgeList[count].dest == node2){
+			adjList[node1].edgeList.erase(
+				adjList[node1].edgeList.begin() + count);
+		}
+		count++;
+	}
+  }
+  if(getCost(node2, node1) != -1){
+	int count = 0;
+	while(count < adjList[node2].edgeList.size()){
+		if(adjList[node2].edgeList[count].dest == node1){
+			adjList[node2].edgeList.erase(
+				adjList[node2].edgeList.begin() + count);
+		}
+		count++;
+	}
+  }
 }
