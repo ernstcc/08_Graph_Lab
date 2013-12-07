62c7cc14a92004c1c4ff7101d07d00f84122779b
Mingwei Zhong <JohnneyMing@users.noreply.github.com>
2013-12-07 09:07:54 -0800

    Added Depth-first-search method in Graph.h

--- Graph.h
+++ Graph.h
@@ -43,6 +43,10 @@ public:
   //Remove the edge from node1 to node2, and also from node2 to node1.
   // If there are no such edges, then don't do anything.
   void removeEdge(int node1, int node2);
+  
+  //Depth-first-search algorithm, once you find the desired node, return the cost.
+  int DFS(int node2);
+
 
 private:
   std::vector<Node> adjList;	
