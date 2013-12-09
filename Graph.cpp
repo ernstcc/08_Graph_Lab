9dee3da5c2cf741084aaed0c9d9c7dce89e7f426
deckerbd <deckerbd@miamioh.edu>
2013-12-08 23:59:35 -0500

    Fixed tabbing throughout project; getCost seems to be working

--- Graph.cpp
+++ Graph.cpp
@@ -6,27 +6,36 @@
 #include "Graph.h"
 
 Graph::Graph(unsigned int numNodes){
-  //TODO
-  // Learned about vector's std methods from zhongm2 (http://www.cplusplus.com/reference/vector/vector/resize/)(https://github.com/JohnneyMing)
-  adjList.resize(numNodes);
+	//TODO
+	// Learned about vector's std methods from zhongm2 (http://www.cplusplus.com/reference/vector/vector/resize/)(https://github.com/JohnneyMing)
+	adjList.resize(numNodes);
 }
 
 int Graph::getCost(int node1, int node2){
-  //TODO
-  return -1;
+	//TODO
+	// Return the value when edgeList==node2
+	// If such point doesn't exist, return -1;
+	
+	for(int i=0; i<adjList[node1].edgeList.size(); i++)
+	{
+		if(adjList[node1].edgeList[i].dest==node2)
+			return adjList[node1].edgeList[i].cost;
+	}
+
+	return -1;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
-  // Put the new edges at the end of each edgeList
-  adjList[node1].edgeList.push_back(Edge(cost, node2));
-  adjList[node2].edgeList.push_back(Edge(cost, node1));
+	//TODO
+	// Put the new edges at the end of each edgeList
+	adjList[node1].edgeList.push_back(Edge(cost, node2));
+	adjList[node2].edgeList.push_back(Edge(cost, node1));
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+	//TODO
 }
